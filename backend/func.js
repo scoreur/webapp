72c97d0d11294e4b42dc818b9d674a1aa04cc9be
Danny Chen <chenxiaoqino@gmail.com>
2014-08-03 01:00:23 +0800

    Prepare for wavgen web

---
+++ backend/func.js
@@ -0,0 +1,13 @@
+WAVGEN={
+	//Read the scores and save into wav file.
+	saveScoreSequence=function(scores)
+	{
+	
+	}
+	
+	//Turn regular array to UInt8 Array Buffer
+	formatter=function(array)
+	{
+	
+	}
+}
