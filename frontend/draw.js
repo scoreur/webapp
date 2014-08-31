873fca196df4ebf462731e4e773500619cb1a1c5
Danny Chen <chenxiaoqino@gmail.com>
2014-08-31 15:40:30 +0800

    initial version of adding notes

--- frontend/draw.js
+++ frontend/draw.js
@@ -131,10 +131,6 @@ LINER={
 			}
 		}
 		
-		elem.onmousedown=LINER.mouse_handlers.global_down;
-		elem.onmouseup=LINER.mouse_handlers.global_up;
-		
-		var lineend=LINER.settings.initial_padding+LINER.settings.scorewidth*LINER.settings.beatsperseq*LINER.settings.seqperline;
 		
 		elem.svg=SVG(elem);
 		
@@ -161,6 +157,7 @@ LINER={
 		elem.svg.upper.addlines=elem.svg.upper.nested();
 		elem.svg.lower.addlines=elem.svg.lower.nested();
 		
+		var lineend=LINER.settings.initial_padding+LINER.settings.scorewidth*LINER.settings.beatsperseq*LINER.settings.seqperline;
 		for(var i=1;i<=5;i++)
 		{
 			var lineheight=LINER.y_conversion(i);
@@ -183,6 +180,24 @@ LINER={
 		{
 			var x=LINER.settings.initial_padding+LINER.settings.scorewidth*LINER.settings.beatsperseq*i;
 			elem.svg.lower.line(x,low,x,high).stroke('grey');
+		}
+		
+		
+		elem.onmousedown=LINER.mouse_handlers.global_down;
+		elem.onmouseup=LINER.mouse_handlers.global_up;
+		
+		elem.svg.lower.rect(lineend,LINER.settings.lineheight*5).center(lineend/2,LINER.y_conversion(-3)).opacity(0);
+		elem.svg.lower.node.onmouseup=function(e){
+			var c=LINER.SVG_transxy(elem.svg.lower.node,e.clientX,e.clientY);
+			var x=LINER.inverse_x_conversion(c[0]),y=LINER.inverse_y_conversion(c[1]);
+			if(x>=0&& x< LINER.settings.beatsperseq*LINER.settings.seqperline){
+				//if()return;//todo: occupied
+				elem.liner.addscore({
+					time:x,
+					duration:1,
+					frnum:LINER.inverse_line_conversion(y)
+				});
+			}
 		}
 		
 		elem.newnote=function(duration){
